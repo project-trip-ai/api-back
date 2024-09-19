8f424aa892611e2c83b2075ab5ecc52d48f25641
Ha Lan D <dnhalan0101@gmail.com>
2024-09-19 03:54:14 +0200

    add search places

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -4,9 +4,12 @@ import {
   VertexAI,
 } from '@google-cloud/vertexai';
 import dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 const project = process.env.PROJECT;
 const location = process.env.LOCATION;
+const apiKey = process.env.API_KEY;
+const endpoint = `https://places.googleapis.com/v1/places:searchText`;
 
 const vertexAI = new VertexAI({
   project: project,
@@ -50,5 +53,52 @@ export async function generateActivities(req, res) {
   } catch (error) {
     console.error(error);
     res.status(500).json({error: 'Failed to fetch from google'});
+  }
+}
+
+export async function searchPlaces(req, res) {
+  const {query} = req.body;
+
+  if (!query) {
+    return res.status(400).json({error: 'Query is required'});
+  }
+
+  try {
+    const response = await axios.post(
+      endpoint,
+      {
+        textQuery: query,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Goog-Api-Key': apiKey,
+          'X-Goog-FieldMask': '*',
+        },
+      },
+    );
+
+    return res.status(200).json(response.data.places[0]);
+  } catch (error) {
+    console.error('Error fetching data from Google Places API:', error);
+    return res.status(500).json({error: 'Internal Server Error'});
+  }
+}
+
+export async function getPhoto(req, res) {
+  const {name} = req.params;
+  console.log('test : ', name);
+  if (!name) {
+    return res.status(400).json({error: 'Name is required'});
+  }
+
+  try {
+    const url = `https://places.googleapis.com/v1/${encodeURIComponent(name)}/media?key=${apiKey}&max_height_px=100`;
+    const response = await axios.get(url);
+    console.log('test :', response);
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error('Error fetching data from Google Phto API:', error);
+    return res.status(500).json({error: 'Internal Server Error'});
   }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import {checkout, webhook} from '../controllers/payementController';
-import {generateActivities} from '../controllers/aiController';
+import {
+  generateActivities,
+  searchPlaces,
+  getPhoto,
+} from '../controllers/aiController';
 const emailController = require('../controllers/emailController');
 const router = express.Router();
 router.post('/checkout', checkout);
 router.post('/webhook', webhook);
 router.post('/new-email', emailController.sendEmail);
 router.get('/ai/activities/:location', generateActivities);
-
+router.post('/ai/search', searchPlaces);
+// router.get('/ai/getPhoto/:name', getPhoto);
 export default router;
