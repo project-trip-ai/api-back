6bd1ff368a67602d9b32485fe66a36f207dd88e0
Rymuu <sutrils-nozu@outlook.fr>
2024-09-20 18:51:00 +0200

    generateActivities

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import {checkout, webhook} from '../controllers/payementController';
-import {getActivities} from '../controllers/aiController';
+import {
+  getActivities,
+  enrichItineraryWithPlaceDetails,
+} from '../controllers/aiController';
 const emailController = require('../controllers/emailController');
 const router = express.Router();
 router.post('/checkout', checkout);
 router.post('/webhook', webhook);
 router.post('/new-email', emailController.sendEmail);
 router.get('/ai/getActivities/:location', getActivities);
+router.get('/ai/generateActivities/:location', enrichItineraryWithPlaceDetails);
 export default router;
